06cb5441bf4afd1867be01233c905921df93aea7
paraslov <paraslovjm@gmail.com>
2024-02-04 07:48:42 +0600

    6 (feat): add update video route

--- src/repositories/videos-repository.ts
+++ src/repositories/videos-repository.ts
@@ -6,10 +6,8 @@ export const videosRepository = {
   async getVideos() {
     return db.videos
   },
-  async getVideoById(id?: number) {
-    if (typeof id !== 'number') return
-
-    return db.videos.find((video) => video.id === id)
+  async getVideoById(id: number) {
+    return this._findVideoById(id)
   },
   async createVideo(payload: VideosBodyModel) {
     const createdVideo = this._mapCreatedVideo(payload)
@@ -17,6 +15,24 @@ export const videosRepository = {
     db.videos.push(createdVideo)
 
     return createdVideo
+  },
+  async updateVideo(id: number, payload: VideosBodyModel) {
+    const updatedVideo = this._findVideoById(id)
+
+    if (!updatedVideo) return false
+
+    db.videos = db.videos.map((video) => {
+      if (video.id === id) {
+        return {
+          ...video,
+          canBeDownloaded: false,
+          ...payload,
+        }
+      }
+      return video
+    })
+
+    return true
   },
   _mapCreatedVideo(payload: VideosBodyModel): VideosViewModel {
     const { title, author, availableResolutions } = payload
@@ -31,5 +47,8 @@ export const videosRepository = {
       createdAt: new Date().toDateString(),
       publicationDate: new Date().toDateString(),
     }
+  },
+  _findVideoById(id: number) {
+    return db.videos.find((video) => video.id === id)
   }
 }
--- src/validations/videosValidations.ts
+++ src/validations/videosValidations.ts
@@ -3,6 +3,20 @@ import { createStringChain } from './index'
 
 export const videosValidations = {
   title: createStringChain('title').isLength({ max: 40 }).withMessage('Title should be from 1 to 40 symbols'),
-  author: createStringChain('author').isLength({ max: 20 }).withMessage('Title should be from 1 to 20 symbols'),
-  availableResolutions: body('availableResolutions').optional().isArray({ min: 1 }),
+  author: createStringChain('author').isLength({ max: 20 }).withMessage('Author should be from 1 to 20 symbols'),
+  availableResolutions: body('availableResolutions').optional({ nullable: true }).isArray({ min: 1 }).withMessage('Resolutions should not be empty'),
+  canBeDownloaded: body('canBeDownloaded').optional().isBoolean().withMessage('Should be boolean'),
+  minAgeRestriction: body('minAgeRestriction').optional({ nullable: true }).custom(isNumber({ min: 1, max: 18 })).withMessage('Restricted age should be between 1 and 18'),
+  publicationDate: body('publicationDate').optional().isString().withMessage('Should be a string'),
+}
+
+function isNumber(options?: { min?: number, max?: number }) {
+  return function (value: any) {
+    if (typeof value !== 'number') return false
+
+    let min = options?.min ?? value
+    let max = options?.max ?? value
+
+    return value >= min && value <= max
+  }
 }
